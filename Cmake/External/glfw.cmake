4e0d58a51af86a49169a1a743f19ca8265283ebf
THISISAGOODNAME <yangyj19931231@gmail.com>
2022-05-21 21:23:21 +0800

    Update external cmake

---
+++ Cmake/External/glfw.cmake
@@ -0,0 +1,7 @@
+set(GLFW_BUILD_DOCS OFF CACHE BOOL "" FORCE)
+set(GLFW_BUILD_EXAMPLES OFF CACHE BOOL "" FORCE)
+set(GLFW_BUILD_TESTS OFF CACHE BOOL "" FORCE)
+set(GLFW_INSTALL OFF CACHE BOOL "" FORCE)
+set(GLFW_VULKAN_STATIC ON CACHE BOOL "" FORCE)
+add_subdirectory(External/glfw)
+set_target_properties(glfw update_mappings PROPERTIES FOLDER "External/glfw")
---
+++ Cmake/External/mimalloc.cmake
@@ -0,0 +1,2 @@
+add_subdirectory(External/mimalloc)
+set_target_properties(mimalloc-static PROPERTIES FOLDER "External/mimalloc")
